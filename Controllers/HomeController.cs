cecc3093db54e22fc75725e61a45dc94ee09507e
PK <monpushp@gmail.com>
2024-09-01 20:25:29 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProAspnetCore.Models;
+
+namespace ProAspnetCore.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        int hour = DateTime.Now.Hour;
+        string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+        return View("MyView", viewModel);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
